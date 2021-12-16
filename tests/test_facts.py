72ff05a3fb5604986cc2e80823b2a08ff2158841
Meg4Bit <pe4enko111@rambler.ru>
2021-12-16 09:37:38 +0000

    Facts test

--- tests/test_facts.py
+++ tests/test_facts.py
@@ -15,21 +15,37 @@ client = TestClient(app)
 
 
 facts = [
-{
-    "name": "Req Square",
-    "description": "Red Square was built in 16-th century",
-    "fact_id": "moscow_red_sqr",
-    "pos": { "type": "Point", "coordinates": [37.620302, 55.754131] },
-  },
-  {
-    "name": "Christ the Saviour Cathedral",
-    "description": "The most important and the largest cathedral in Russia",
-    "fact_id": "moscow_chr_sav_cath",
-    "pos": { "type": "Point", "coordinates": [37.6054939, 55.7446375] },
-  },
+    {
+        "name": "Req Square",
+        "description": "Red Square was built in 16-th century",
+        "fact_id": "moscow_red_sqr",
+        "pos": { "type": "Point", "coordinates": [37.620302, 55.754131] },
+    },
+    {
+        "name": "Christ the Saviour Cathedral",
+        "description": "The most important and the largest cathedral in Russia",
+        "fact_id": "moscow_chr_sav_cath",
+        "pos": { "type": "Point", "coordinates": [37.6054939, 55.7446375] },
+    },
 ]
 
+
+json_facts = []
+for fact in facts:
+    fact["pos"] = fact["pos"]["coordinates"]
+    json_facts.append(fact)
+
+
+new_fact = {
+    "name": "City Hill",
+    "description": "City Hill is a five hectare landscaped hill located in the centre of Canberra and surrounded by Vernon Circle",
+    "fact_id": "city_hill_Canberra",
+    "pos": { "type": "Point", "coordinates": [149.12383, -35.27583] }
+}
+
+
 collection = mongomock.MongoClient().db.collection
+
 
 for obj in facts:
     collection.insert_one(obj)
@@ -48,12 +64,35 @@ def test_get_facts():
     response = client.get("/facts")
 
     assert response.status_code == 200
-    assert response.json() == [facts[0]]
+    assert response.json() == json_facts
 
 
-# def test_get_facts_by_id():
-#     for fact in facts:
-#         response = client.get(f"/facts/{fact['fact_id']}")
+def test_get_fact_by_id():
+    for fact in facts:
+        response = client.get(f"/facts/{fact['fact_id']}")
 
-#         assert response.status_code == 200
-#         assert response.json() == fact
+        assert response.status_code == 200
+        assert response.json() == json_facts
+
+
+def test_post_fact():
+    response = client.post(
+        "/facts",
+        json=new_fact,
+    )
+    assert response.status_code == 200
+    assert response.json() == None
+
+    response = client.get(f"/facts/{new_fact['fact_id']}")
+    assert response.status_code == 200
+    assert response.json() == new_fact
+
+
+def test_post_duplicat_fact():
+    response = client.post(
+        "/facts",
+        json=facts[0],
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "fact ID occupied"}
+
